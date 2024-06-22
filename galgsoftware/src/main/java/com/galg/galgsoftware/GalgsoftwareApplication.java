ed42a17a0e201e7b7e743d2b0e5753eeb25e1c7f
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-06-22 11:21:29 -0300

    [init] Importando spring project

---
+++ galgsoftware/src/main/java/com/galg/galgsoftware/GalgsoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.galg.galgsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalgsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalgsoftwareApplication.class, args);
+	}
+
+}
---
+++ galgsoftware/src/test/java/com/galg/galgsoftware/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.galg.galgsoftware;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
